7394840ef3e11d195e509d07cd8b5613025a3830
Your Name <your-email@example.com>
2026-04-09 09:42:24 +0200

    update project

---
+++ app/config/database.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* ============================================================
+* DATABASE.PHP
+* Database configuratie-array
+* ============================================================
+*/
+return [
+    'host' => DB_HOST,
+    'dbname' => DB_NAME,
+    'user' => DB_USER,
+    'pass' => DB_PASS,
+    'charset' => 'utf8mb4'
+];
