bedda1edb8b3882ccaa70288714839321df37ad9
Daniello <daniello@smilepro.com>
2026-04-09 13:14:11 +0200

    feat: add leverancier find and update model methods

--- app/models/Leverancier.php
+++ app/models/Leverancier.php
@@ -59,6 +59,46 @@ class Leverancier extends BaseModel
 		return (int)$statement->fetchColumn() > 0;
 	}
 
+	public function existsByBedrijfsnaamExceptId(string $bedrijfsnaam, int $excludeId): bool
+	{
+		$statement = $this->db->prepare('SELECT COUNT(*) FROM leveranciers WHERE bedrijfsnaam = ? AND leverancier_id != ?');
+		$statement->execute([$bedrijfsnaam, $excludeId]);
+
+		return (int)$statement->fetchColumn() > 0;
+	}
+
+	public function findById(int $id): array|false
+	{
+		try {
+			$statement = $this->db->prepare('CALL sp_leverancier_ophalen(?)');
+			$statement->execute([$id]);
+			$result = $statement->fetch();
+			$statement->closeCursor();
+
+			return $result;
+		} catch (Throwable $exception) {
+			$sql = '
+				SELECT
+					leverancier_id,
+					bedrijfsnaam,
+					adres,
+					postcode,
+					plaats,
+					contactpersoon,
+					email_contact,
+					telefoon,
+					eerstvolgende_levering
+				FROM leveranciers
+				WHERE leverancier_id = ?
+			';
+
+			$statement = $this->db->prepare($sql);
+			$statement->execute([$id]);
+
+			return $statement->fetch();
+		}
+	}
+
 	public function create(array $data): int
 	{
 		try {
@@ -104,6 +144,54 @@ class Leverancier extends BaseModel
 			]);
 
 			return (int)$this->db->lastInsertId();
+		}
+	}
+
+	public function update(int $id, array $data): bool
+	{
+		try {
+			$statement = $this->db->prepare('CALL sp_leverancier_wijzigen(?, ?, ?, ?, ?, ?, ?, ?, ?)');
+			$statement->execute([
+				$id,
+				$data['bedrijfsnaam'],
+				$data['adres'],
+				$data['postcode'],
+				$data['plaats'],
+				$data['contactpersoon'],
+				$data['email_contact'],
+				$data['telefoon'],
+				$data['eerstvolgende_levering'],
+			]);
+			$statement->closeCursor();
+
+			return true;
+		} catch (Throwable $exception) {
+			$sql = '
+				UPDATE leveranciers
+				SET
+					bedrijfsnaam = ?,
+					adres = ?,
+					postcode = ?,
+					plaats = ?,
+					contactpersoon = ?,
+					email_contact = ?,
+					telefoon = ?,
+					eerstvolgende_levering = ?
+				WHERE leverancier_id = ?
+			';
+
+			$statement = $this->db->prepare($sql);
+			return $statement->execute([
+				$data['bedrijfsnaam'],
+				$data['adres'],
+				$data['postcode'],
+				$data['plaats'],
+				$data['contactpersoon'],
+				$data['email_contact'],
+				$data['telefoon'],
+				$data['eerstvolgende_levering'],
+				$id,
+			]);
 		}
 	}
 }
